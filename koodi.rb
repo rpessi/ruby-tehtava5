651d891d5ed6ecef1f6f8da2f334a7d1aff645b5
Riitta Pessi <riitta@pessi.fi>
2025-03-14 14:49:55 +0200

    Ruby harjoittelutehtävä 5

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,11 @@
-# kirjoita koodi tänne
+# Tee metodi puuttuva, joka saa parametrina taulukon, jonka pituus on n. 
+# Taulukossa on kertaalleen yhtä lukuunottamatta jokainen välin 0..n luku. 
+# Metodin tulee palauttaa puuttuva luku.
+
+def puuttuva(taulukko)
+  taulukko.length.times do |i|
+    return i if not taulukko.include? i
+  end
+end
+
+
